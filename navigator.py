6fa421fcc8e7338cc5558c7cfe89113de076c86a
Tallerik <tallerik@users.noreply.github.com>
2025-03-14 16:25:02 +0100

    Updated navigator: Added parameters for configs, added calculation for turning time. Added comments

--- navigator.py
+++ navigator.py
@@ -4,42 +4,59 @@ from time import sleep
 
 class Navigator:
     
-    def __init__(self, x = 0.0, y = 0.0):
-        self.current = Position(x,y)
+    def __init__(self, left_motor = "M1", right_motor = "M0", speed_factor = 0.8, turn_factor = 0.3, start_x = 0.0, start_y = 0.0):
+        self.initial = Position(start_x,start_y)
+        self.current = Position(start_x, start_y)
         self.target = Position()
-        self.speed_factor = 0.8
+
         self.action = Action.IDLE
 
-        self.drive = DriveController(left_motor_port="M1", right_motor_port="M0")
+        self.speed_factor = speed_factor
+        self.turn_factor = turn_factor
+
+        self.drive = DriveController(left_motor_port=left_motor, right_motor_port=right_motor)
         self.max_speed = self.drive.max_motor_speed
         
 
     def goto(self, x,y):
-        
         self.target = Position(x,y)
         self.action = Action.GOTO
 
+        # Calculate Distance and Heading
         dis = self.current.distanceTo(self.target)
-        turn = self.current.deltaHeadingTo(self.target)
+        turn_angle = self.current.deltaHeadingTo(self.target)
 
-        self.drive.rotate(angle=-turn, time_to_take=1)
-        sleep(1)
+        # Turn towards new heading
+        self.drive.rotate(angle=-turn_angle, max_speed_factor=self.turn_factor) # Turn
+        sleep(self._calculate_turning_duration(turn_angle)) # Wait for the duration of the turn
         self.current.setHeadingTo(self.target) # Sets the heading of current to the new heading
-        
-        self.drive.forward(1, True, dis)
-        sleep(self._calculateDuration(dis))
+
+        # Drive forward
+        self.drive.forward(self.speed_factor, True, dis)
+        sleep(self._calculate_driving_duration(dis)) # Wait for driving to complete
+
+        # Done driving. Save settings and clean up
         self.target.heading = self.current.heading # Save the heading
         self.current = self.target # New current Position is now the old target Position 
-        self.target = Position() # Reset target
-        print(self.current.toString())
-        self.action = Action.IDLE
-        
 
-    def _calculateDuration(self, distance):
+        # Reset target position
+        self.target = Position()
+        # Done, reset action
+        self.action = Action.IDLE
+
+    def go_home(self):
+        self.goto(self.initial.x, self.initial.y)
+
+    def _calculate_driving_duration(self, distance):
         self.max_speed = self.drive.max_motor_speed # Maybe this can Change?
         target_speed = self.max_speed * self.speed_factor
 
         return distance / target_speed
+
+    def _calculate_turning_duration(self, angle):
+        angle = abs(angle)
+        rotation_speed = self.drive.max_robot_angular_speed * self.turn_factor
+        return angle / rotation_speed
 
 
 class Action:

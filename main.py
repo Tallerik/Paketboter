c34c5fa28f2cac20f7299d5183b6c2920bce2b09
Erik <erik.liederbach@bszn-da.de>
2025-03-21 12:22:39 +0100

    Implemented different Methods to main

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from lib.remote_driving import RemoteDriving
 from lib.distance_protection import DistanceProtection
+from navigator import Navigator
 #from bluedot_drive import BlueDotDrive
 from time import sleep
 
 
 dr = RemoteDriving()
-
+#nav = Navigator(turn_factor = 0.1, speed_factor=0.5)
 #drive = BlueDotDrive(autostart=False)
 prot = DistanceProtection(stopper = dr)
 
@@ -13,3 +14,7 @@ prot = DistanceProtection(stopper = dr)
 
 while True:
     dr.listen()
+    #nav.goto (0, 1)
+    #nav.goto(1, 1)
+    #nav.goto(1, 0.1)
+    #nav.goto(0, 0)
